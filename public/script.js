7cf05ef40dfc12a4f0797a4f1b5360e85bb33073
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2024-03-21 21:18:43 -0400

    render

--- public/script.js
+++ public/script.js
@@ -18,29 +18,93 @@ const showCrafts = async () => {
     }
 
     let craftDiv = document.getElementById("craft-list");
+    let columns = [];
+    for (let i = 0; i < 4; i++) {
+        let column = document.createElement("div");
+        column.className = "column";
+        craftDiv.append(column);
+        columns.push(column);
+    }
 
-    craftJSON.forEach((craft) => {
-        let section = document.createElement("section");
-        craftDiv.append(section);
-        let title = craft.name;
+    craftJSON.forEach((craft,index) => {
 
-        let h3 = document.createElement("h3");
-        section.append(h3);
-        h3.innerHTML = craft.name;
-
+        let column = columns[index % 4];
         let img = document.createElement("img");
-        section.append(img);
-        // img.src="http://localhost:3000/images/crafts/" + craft.image; 
         img.src="images/" + craft.image; 
+        img.onclick = () => openModalWithCraft(craft); 
+        column.append(img);
+        // "img.src="images/" + craft.image; THIS IS FOR RENDER
 
+        function openModalWithCraft(craft) {
+            var modal = document.getElementById("myModal");
+            var modalContent = document.querySelector(".modal-content");
+        
+            //clear modal so it doesnt stack
+            var existingFlexContainer = modalContent.querySelector(".modal-flex-container");
+            if (existingFlexContainer) {
+                existingFlexContainer.remove(); 
+            }
+        
+            //flex container and append data
+            var flexContainer = document.createElement("div");
+            flexContainer.className = "modal-flex-container";
+        
+            var imgDiv = document.createElement("div");
+            imgDiv.className = "modal-img-container";
+            var img = document.createElement("img");
+            img.src="images/" + craft.image; 
+            imgDiv.append(img);
+            flexContainer.append(imgDiv);
+        
+            var textContentDiv = document.createElement("div");
+            textContentDiv.className = "modal-text-container";
+        
+            var name = document.createElement("h2");
+            name.textContent = craft.name;
+            textContentDiv.append(name);
+        
+            var description = document.createElement("p");
+            description.textContent = craft.description;
+            textContentDiv.append(description);
 
-        let description = document.createElement("p");
-        section.append(description);
-        description.innerHTML = craft.description;
-
-        // let supplies = document.createElement("ul");
-        // section.append(ul);
-
+            var supplies = document.createElement("h2");
+            supplies.innerHTML = "Supplies"
+            textContentDiv.append(supplies);
+        
+            if (craft.supplies.length) {
+                var ul = document.createElement("ul");
+                craft.supplies.forEach(supply => {
+                    var li = document.createElement("li");
+                    li.textContent = supply;
+                    ul.append(li);
+                });
+                textContentDiv.append(ul);
+            }
+        
+            flexContainer.append(textContentDiv);
+            modalContent.append(flexContainer);
+        
+            modal.style.display = "block";
+        }
+        
+       
+        function closeModal() {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "none";
+        }
+        
+        var span = document.querySelector(".modal-content .close");
+        if (span) {
+            span.onclick = closeModal;
+        }
+        
+        window.onclick = function(event) {
+            var modal = document.getElementById("myModal");
+            if (event.target == modal) {
+                closeModal(); 
+            }
+        };
+        
     });
 };
 
